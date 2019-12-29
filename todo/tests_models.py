155cc7e691bb749a35f152ccea7c2d4414cb6894
Rhy <yhr@xs4all.nl>
2019-12-29 15:33:52 +0000

    expanded test functionality - added coverage

--- todo/tests_models.py
+++ todo/tests_models.py
@@ -2,7 +2,18 @@ from django.test import TestCase
 
 # Create your tests here.
 
-class TestDjango (TestCase):
+from .models import Item
 
-    def test_is_this_thing_on(self):
-        self.assertEqual(1, 1)
+class TestItemModel(TestCase):
+
+    def test_done_defaults_to_False(self):
+        item = Item(name="Create a Test")
+        item.save()
+        self.assertEqual(item.name, "Create a Test")
+        self.assertFalse(item.done)
+    
+    def test_can_create_an_item_with_a_name_and_status(self):
+        item = Item(name="Create a Test", done=True)
+        item.save()
+        self.assertEqual(item.name, "Create a Test")
+        self.assertTrue(item.done)
